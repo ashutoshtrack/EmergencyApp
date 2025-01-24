48e5343052594a85742503b0e6415f35d48da87e
Ashutosh Telang <ashutosh.telang@gajigesa.com>
2025-01-25 00:17:03 +0530

    task: test ddeployment

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Amplify} from 'aws-amplify';
-// import awsconfig from './aws-exports'; // Path to auto-generated config file
+// import awsconfig from './aws-exports'; // Path to auto-generated config  file
 
 Amplify.configure({
   aws_project_region: 'ap-south-1',
