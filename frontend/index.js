8a69790429f022914cb414e013077d1c6561780e
Ashutosh Telang <ashutosh.telang@gajigesa.com>
2025-01-24 23:24:26 +0530

    task: updated configure

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Amplify} from 'aws-amplify';
-import awsconfig from './aws-exports'; // Path to auto-generated config file
+// import awsconfig from './aws-exports'; // Path to auto-generated config file
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  aws_project_region: 'ap-south-1',
+});
 AppRegistry.registerComponent(appName, () => App);
